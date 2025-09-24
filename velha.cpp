2d4d12ee009e1c0d507fed99ec55c7576fd0e7cf
Arthur Soares Magalhães Freire <arthursmfreire@gmail.com>
2025-09-24 13:21:41 -0300

    Correção do test case 2

--- velha.cpp
+++ velha.cpp
@@ -7,15 +7,20 @@
 
 /** 
  * @brief verifica situacao do jogo da velha  
- * @author Programador 
- * @param  velha descreve o parametro
+ * @author Arthur Soares  
+ * @param  velha é uma matriz 3x3 que representa a situação atual do jogo da velha
  * 
- *  Descrever o que a funcao faz
+ *  A função analisa se o jogo da velha representado pela matriz é válido ou não,
+ *  retornando 1 para vitória do jogador 1 e 2 para vitória do jogador 2, 0 para empate, 
+ *  -1 para inváido e -2 para jogos impossíveis.
  */ 
 
 int VerificaVelha( int velha[3][3] )
 {
 	if(velha[0][2] == 1 && velha[1][2] == 1 && velha[2][2] == 1){
+		return 1; /*!< retorna 1 para jogo válido */
+	}
+	if(velha[0][1] == 1 && velha[1][1] == 1 && velha[2][1] == 1){
 		return 1; /*!< retorna 1 para jogo válido */
 	}
 	return 0; /*!< retorna zero para teste */ 
