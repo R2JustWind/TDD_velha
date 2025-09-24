a9aedd8ce0fa7d1d47edd7703ae887175df005be
Arthur Soares Magalhães Freire <arthursmfreire@gmail.com>
2025-09-24 16:42:00 -0300

    Refatoração dos casos em que o jogador 2 ganha na linha

--- velha.cpp
+++ velha.cpp
@@ -31,6 +31,10 @@ int VerificaVelha(int velha[3][3]) {
             velha[i][2] == 1) {
                 return 1; /*!< retorna 1 para vitória do jogador 1 */
         }
+		if (velha[i][0] == velha[i][1] && velha[i][1] == velha[i][2] &&
+            velha[i][2] == 2) {
+                return 2; /*!< retorna 2 para vitória do jogador 1 */
+        }
     }
     if (velha[0][0] == velha[1][1] && velha[1][1] == velha[2][2] &&
         velha[2][2] == 1) {
@@ -40,15 +44,6 @@ int VerificaVelha(int velha[3][3]) {
         velha[2][0] == 1) {
         return 1;
     }
-	if (velha[0][0] == velha[0][1] && velha[0][1] == velha[0][2] && velha[0][2] == 2) {
-		return 2;
-	}
-	if (velha[1][0] == velha[1][1] && velha[1][1] == velha[1][2] && velha[1][2] == 2) {
-		return 2;
-	}
-	if (velha[2][0] == velha[2][1] && velha[2][1] == velha[2][2] && velha[2][2] == 2) {
-		return 2;
-	}
 
     return 0; /*!< retorna zero para teste */
 }
