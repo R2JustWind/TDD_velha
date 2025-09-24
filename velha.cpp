6199662721b3f242e134b50e350e0d0837b2fb30
Arthur Soares Magalhães Freire <arthursmfreire@gmail.com>
2025-09-24 16:24:03 -0300

    Correção do test case 11

--- velha.cpp
+++ velha.cpp
@@ -42,6 +42,9 @@ int VerificaVelha(int velha[3][3]) {
 	if (velha[0][1] == velha[1][1] && velha[1][1] == velha[2][1] && velha[2][1] == 2){
 		return 2;
 	}
+	if (velha[0][0] == velha[1][0] && velha[1][0] == velha[2][0] && velha[2][0] == 2){
+		return 2;
+	}
 
     return 0; /*!< retorna zero para teste */
 }
